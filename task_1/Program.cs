c48293e570fd85e272abf45b77ce96eecd470781
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-03-12 15:21:02 +0300

    Add task 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N.
+// Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+int inputInt(string msg)
+{
+    int value;
+    System.Console.Write($"{msg} -> ");
+    if (int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Введенно неверное значение!");
+    Environment.Exit(999);
+    return 0;
+}
+
+void ShowEvenNumbers (int numberFrom, int numberTo)
+{
+    if (numberTo < numberFrom)
+    {
+        return;
+    }
+
+    ShowEvenNumbers(numberFrom, numberTo - 1);
+    
+    if (numberTo %2 ==0)
+    {
+        System.Console.Write($"{numberTo} ");
+    }
+}
+
+int m = inputInt("Введите первое число");
+int n = inputInt("Введите второе число");
+
+ShowEvenNumbers(m, n);
+
